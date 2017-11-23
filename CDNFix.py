e3156b8e7ec94ea77ab0cbd7c27f2e205ee561df
Lain <0xlain@protonmail.com>
2017-11-23 15:34:01 +0100

    feat(main:launcher): created. Lowers privilege.
    .

--- CDNFix.py
+++ CDNFix.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
+import os
+
+
+def elevate_privilege():
+  os.seteuid(0)
+
+def lower_privilege():
+  os.seteuid(65534)
+
+
+if __name__ == "__main__":
+
+
+  if os.getuid() != 0:
+    print("You need to be root to run this program.")
+    exit()
+  # we lower privelege everywhere.
+  # we will upper privilege only when needed.
+  lower_privilege()
